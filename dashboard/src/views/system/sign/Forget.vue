d00bd884f67d17e576b6237380e2e3ab6888cba3
张瑞 <zhangr@zjcds.com>
2021-07-31 00:21:10 +0800

    修复登录注册相关页面

---
+++ dashboard/src/views/system/sign/Forget.vue
@@ -0,0 +1,155 @@
+<template>
+  <a-form class="atom-sign-form" :model="forgetUser" :rules="forgetRules">
+    <a-form-item name="account">
+      <a-input v-model:value="forgetUser.account" placeholder="请输入帐号" size="large" allowClear>
+        <template #prefix>
+          <IconFont type="UserOutlined"/>
+        </template>
+      </a-input>
+    </a-form-item>
+    <a-form-item name="phone">
+      <a-input v-model:value="forgetUser.phone" placeholder="请输入手机号" size="large" allowClear>
+        <template #prefix>
+          <IconFont type="MobileOutlined"/>
+        </template>
+      </a-input>
+    </a-form-item>
+    <a-form-item name="verifyCode" class="atom-form-flex">
+      <a-input v-model:value="forgetUser.verifyCode" placeholder="请输入验证码" size="large" allowClear>
+        <template #prefix>
+          <IconFont type="MessageOutlined"/>
+        </template>
+      </a-input>
+      <a-button class="atom-form-flex-right"
+                type="primary"
+                ghost
+                @click="sendVerifyCode"
+                size="large"
+                :disabled="timeOut !== null">
+        <span v-if="!timeOut">发送验证码</span>
+        <a-statistic-countdown v-else
+                               :value="timeOut"
+                               prefix="已发送"
+                               suffix="秒"
+                               format="ss"
+                               @finish="onFinished"></a-statistic-countdown>
+      </a-button>
+    </a-form-item>
+    <a-form-item v-if="device.isMobile" name="password">
+      <a-input-password v-model:value="forgetUser.password" placeholder="请输入密码" size="large" @change="handlePasswordChange" allowClear>
+        <template #prefix>
+          <IconFont type="LockOutlined"/>
+        </template>
+      </a-input-password>
+      <template #help>
+        <div :class="['atom-password-level', passwordLevelEnum[passwordLevel].class]">
+          强度：<span class="atom-password-level-desc">{{ passwordLevelEnum[passwordLevel].title }}</span>
+        </div>
+        <a-progress :percent="passwordLevelEnum[passwordLevel].percent"
+                    :showInfo="false"
+                    size="small"
+                    :strokeColor="passwordLevelEnum[passwordLevel].color"></a-progress>
+        <div>密码至少8位【包括大小写字母、数字】</div>
+      </template>
+    </a-form-item>
+    <a-form-item v-else name="password">
+      <a-popover placement="left">
+        <a-input-password v-model:value="forgetUser.password" placeholder="请输入密码" size="large" @change="handlePasswordChange" allowClear>
+          <template #prefix>
+            <IconFont type="LockOutlined"/>
+          </template>
+        </a-input-password>
+        <template #content>
+          <div :class="['atom-password-level', passwordLevelEnum[passwordLevel].class]">
+            强度：<span class="atom-password-level-desc">{{ passwordLevelEnum[passwordLevel].title }}</span>
+          </div>
+          <a-progress :percent="passwordLevelEnum[passwordLevel].percent"
+                      :showInfo="false"
+                      size="small"
+                      :strokeColor="passwordLevelEnum[passwordLevel].color"></a-progress>
+          <div>密码至少8位【包括大小写字母、数字】</div>
+        </template>
+      </a-popover>
+    </a-form-item>
+    <a-form-item>
+      <a-button type="primary" :loading="loading" size="large" block @click="handleForget">重置密码</a-button>
+    </a-form-item>
+    <a-form-item class="atom-form-flex">
+      其他登录：
+      <a-avatar-group>
+        <a-avatar><template #icon><IconFont type="QqOutlined"/></template></a-avatar>
+        <a-avatar><template #icon><IconFont type="WechatOutlined"/></template></a-avatar>
+        <a-avatar><template #icon><IconFont type="AlipayCircleOutlined"/></template></a-avatar>
+      </a-avatar-group>
+      <a-button type="link" @click="handleSignIn">已有帐号登录</a-button>
+    </a-form-item>
+  </a-form>
+</template>
+
+<script>
+import FieldRules from '@/components/Common/FuncForm/Form/mixins/FieldRules'
+import config from '@/config/mixins/config'
+export default {
+  name: 'Forget',
+  mixins: [config],
+  data () {
+    return {
+      forgetUser: {
+        // 帐号信息
+        account: '',
+        // 手机号码
+        phone: '',
+        // 密码
+        password: '',
+        // 验证码
+        verifyCode: ''
+      },
+      // 注册校验规则
+      forgetRules: {
+        account: [{ trigger: 'blur', required: true, message: '请输入帐号' }],
+        phone: [{ trigger: 'blur', required: true, pattern: FieldRules.mobilePattern, message: '请输入手机号' }],
+        verifyCode: [{ trigger: 'blur', required: true, message: '请输入验证码' }],
+        password: [{ trigger: 'blur', required: true, pattern: FieldRules.passwordPattern, message: '请输入密码' }]
+      },
+      passwordLevelEnum: {
+        1: { title: '低', class: 'low', percent: 33, color: '#f5222d' },
+        2: { title: '中', class: 'middle', percent: 66, color: '#faad14' },
+        3: { title: '高', class: 'strong', percent: 100, color: '#52c41a' }
+      },
+      passwordLevel: 1,
+      // 登录超时时间
+      timeOut: null,
+      // 是否正在加载
+      loading: false
+    }
+  },
+  methods: {
+    // 获取验证码
+    sendVerifyCode () {
+      // 调用后台短信网关
+      this.$api.system.sendVerifyCode(this.forgetUser.phone).then(() => {
+        this.$message.success('验证码发送成功')
+      })
+      this.timeOut = Date.now() + 1000 * 60
+    },
+    // 倒计时结束
+    onFinished () {
+      this.timeOut = null
+    },
+    // 响应密码输入变化
+    handlePasswordChange ($event) {
+      this.passwordLevel = FieldRules.passwordLevel($event.target.value)
+    },
+    // 响应登录
+    handleSignIn () {
+      this.$router.push({ name: 'signIn' })
+    },
+    // 响应注册
+    handleForget () {
+      this.$api.system.forgetPassword(this.forgetUser).then(() => {
+
+      })
+    }
+  }
+}
+</script>
---
+++ dashboard/src/views/system/sign/SignIn.vue
@@ -0,0 +1,138 @@
+<template>
+  <a-form class="atom-sign-form" :model="signUser" :rules="signRules">
+    <a-tabs v-model:activeKey="signUser.type">
+      <a-tab-pane key="account" tab="帐户密码登录">
+        <a-form-item name="account">
+          <a-input v-model="signUser.account" placeholder="请输入帐号" size="large" allowClear>
+            <template #prefix>
+              <IconFont type="UserOutlined"/>
+            </template>
+          </a-input>
+        </a-form-item>
+        <a-form-item name="password">
+          <a-input-password v-model="signUser.password" placeholder="请输入密码" size="large" allowClear>
+            <template #prefix>
+              <IconFont type="LockOutlined"/>
+            </template>
+          </a-input-password>
+        </a-form-item>
+      </a-tab-pane>
+      <a-tab-pane key="phone" tab="手机号登录">
+        <a-form-item name="phone">
+          <a-input v-model="signUser.phone" placeholder="请输入手机号" size="large" allowClear>
+            <template #prefix>
+              <IconFont type="MobileOutlined"/>
+            </template>
+          </a-input>
+        </a-form-item>
+        <a-form-item name="verifyCode" class="atom-form-flex">
+          <a-input v-model="signUser.verifyCode" placeholder="请输入验证码" size="large" allowClear>
+            <template #prefix>
+              <IconFont type="MessageOutlined"/>
+            </template>
+          </a-input>
+          <a-button class="atom-form-flex-right"
+                    type="primary"
+                    ghost
+                    @click="sendVerifyCode"
+                    size="large"
+                    :disabled="timeOut !== null">
+            <span v-if="!timeOut">发送验证码</span>
+            <a-statistic-countdown v-else
+                                   :value="timeOut"
+                                   prefix="已发送"
+                                   suffix="秒"
+                                   format="ss"
+                                   @finish="onFinished"></a-statistic-countdown>
+          </a-button>
+        </a-form-item>
+      </a-tab-pane>
+    </a-tabs>
+    <a-form-item class="atom-form-flex">
+      <a-checkbox>自动登录</a-checkbox>
+      <a-button type="link" @click="handleForget">忘记密码</a-button>
+    </a-form-item>
+    <a-form-item>
+      <a-button type="primary" :loading="loading" size="large" block @click="handleSignIn">登录</a-button>
+    </a-form-item>
+    <a-form-item class="atom-form-flex">
+      其他登录：
+      <a-avatar-group>
+        <a-avatar><template #icon><IconFont type="QqOutlined"/></template></a-avatar>
+        <a-avatar><template #icon><IconFont type="WechatOutlined"/></template></a-avatar>
+        <a-avatar><template #icon><IconFont type="AlipayCircleOutlined"/></template></a-avatar>
+      </a-avatar-group>
+      <a-button type="link" @click="handleSignUp">注册帐号</a-button>
+    </a-form-item>
+  </a-form>
+</template>
+
+<script>
+export default {
+  name: 'SignIn',
+  data () {
+    return {
+      signUser: {
+        // 登录方式
+        type: 'account',
+        // 帐号信息
+        account: '',
+        // 手机号码
+        phone: '',
+        // 密码
+        password: '',
+        // 验证码
+        verifyCode: ''
+      },
+      // 登录超时时间
+      timeOut: null,
+      // 是否正在加载
+      loading: false
+    }
+  },
+  computed: {
+    // 登录校验规则
+    signRules () {
+      if (this.signUser.type === 'account') {
+        return {
+          account: [{ trigger: 'blur', required: true, message: '请输入帐号' }],
+          password: [{ trigger: 'blur', required: true, message: '请输入密码' }]
+        }
+      } else {
+        return {
+          phone: [{ trigger: 'blur', required: true, message: '请输入手机号' }],
+          verifyCode: [{ trigger: 'blur', required: true, message: '请输入验证码' }]
+        }
+      }
+    }
+  },
+  methods: {
+    // 获取验证码
+    sendVerifyCode () {
+      // 调用后台短信网关
+      this.$api.system.sendVerifyCode(this.signUser.phone).then(() => {
+        this.$message.success('验证码发送成功')
+      })
+      this.timeOut = Date.now() + 1000 * 60
+    },
+    // 倒计时结束
+    onFinished () {
+      this.timeOut = null
+    },
+    // 响应登录
+    handleSignIn () {
+      this.$api.system.signIn(this.signUser).then(() => {
+
+      })
+    },
+    // 响应忘记密码
+    handleForget () {
+      this.$router.push({ name: 'forget' })
+    },
+    // 响应注册
+    handleSignUp () {
+      this.$router.push({ name: 'signUp' })
+    }
+  }
+}
+</script>
